3c14231587ad76721ce34e270dc71e5665aa2271
toghrulmammdl <togrul9871@gmail.com>
2025-06-03 12:53:17 +0400

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,22 +5,25 @@ import Catalogue from '../Buttons/Catalogue'
 import SearchBar from '../Forms/SearchBar'
 import LanguageSelection from '../Dropdown/LanguageSelection'
 import Profile from '../Dropdown/Profile'
+import { FaRegHeart } from "react-icons/fa";
+import { BsBasket3 } from "react-icons/bs";
+
 
 const Header = () => {
   return (
     <header>
         <div className={styles.navbar}>
             <div className={styles.logoContainer}>
-                <img src={logoLibraff} alt="" className={styles.logo}/>
+                <img src={logoLibraff} alt="Libraff logo" className={styles.logo}/>
             </div>
             <div><Catalogue /></div>
             <div><SearchBar /></div>
             <div><LanguageSelection /></div>
             <div><Profile /></div>
-            <div>WHISHLIST</div>
-            <div>CART</div>
+            <div><FaRegHeart style={{fontSize: "24px"}}/></div>
+            <div><BsBasket3 style={{fontSize: "24px"}}/></div>
         </div>
-        <div className='bottom'>
+        <div className={styles.bottomNavbar}>
             <div>Bestseller</div>
             <div>Endirimler</div>
             <div>Muellifler</div>
